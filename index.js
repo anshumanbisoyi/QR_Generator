0d33d4af9fe3cf4194676aa4583d63caffeff99e
Anshuman Bisoyi <ansuman746@gmail.com>
2023-06-19 16:22:30 +0530

    intial

---
+++ index.js
@@ -0,0 +1,6 @@
+/*
+Use the inquirer npm package to get user input.
+Use the qr-image npm package to turn the user enterned URL into a QR code.
+Create a txt file to save the user input usnig the native fs node module.
+*/
+
